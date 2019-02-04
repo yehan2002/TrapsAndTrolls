b4498ea2577cddc1ef1023a9ced7f4448cf98328
yehan2002 <Yehan Jaya>
2019-02-04 19:04:04 +0530

    update build scripts again

--- deploy.py
+++ deploy.py
@@ -5,22 +5,28 @@ import os
 if len(sys.argv) == 1:
     quit("Usage: %s [Version]" % os.path.dirname(__file__))
 
-v = sys.argv[1]
+version= sys.argv[1]
 
 os.chdir(os.path.dirname(__file__))
 
-filedata = []
+def addVersion(path, key, data):
+    filedata = []
 
-with open('build.gradle', 'rb') as f:
-    for l in f.readlines():
-        if "//$VERSION$" in l:
-            l = 'version = %s //$VERSION$\n'
-        filedata.append(filedata)
-            
-with open("build.gradle", "wb") as f:
-    f.writelines(l)
+    with open(path, 'rb') as f:
+        for l in f.readlines():
+            if key in l:
+                l = data % version
+            filedata.append(l)
+           
 
-os.system('git tag %s'%v)
+    with open(path, "wb") as f:
+        f.writelines(filedata)
+
+addVersion('buid.gradle', "//$VERSION$", 'version = "%s" //$VERSION$\n')
+addVersion('src/plugin.yml', "#$version$", 'version: %s #$version$\n')
+
+quit()
+os.system('git tag %s'% version)
 os.system('git add -A')
 os.system('git commit')
 os.system('git push origin master --tags')
